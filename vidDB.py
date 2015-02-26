739175b0784d7cfc28c8ebe923394123e16bef62
Vernon Wenberg III <vwenberg@ribbed.us>
2015-02-26 02:14:40 -0600

    Make directory view refresh with new entered directory instead of just appending things to the end of the current directory list.

--- vidDB.py
+++ vidDB.py
@@ -11,6 +11,7 @@ import tkinter.messagebox
 class DrawGui:
 
     def __init__(self, root):
+        self.dirfuncs = DirFuncs()
 
         # START MENU BAR
         self.theMenuBar = tk.Menu(root)
@@ -48,31 +49,33 @@ class DrawGui:
         self.tree.column("Size", width=20)
         self.tree.heading("Filename", text="Filename")
         self.tree.heading("Size", text="Size")
-        # self.tree.insert("", "end", values="test, test1")
         self.tree.grid(row=2, sticky=tk.EW, columnspan=2)
 
     def build_dir_list(self, event):
-        dirfuncs = DirFuncs()
         self.directory_name = self.directory_entry.get()
-        print(self.directory_name)
-        dir_list = dirfuncs.directory_list(self.directory_name)
-        for a in dir_list:
-            self.tree.insert("", "end", values=a)
-        # TODO: Adjust column width to longest filename
-        # TODO: Add size indicator (eg: MB, KB, GB)
-        # self.tree.column("Filename", width=50)
+        for var in self.tree.get_children():
+            self.tree.delete(var)
+
+        dir_list = self.dirfuncs.directory_list(self.directory_name)
+        if dir_list != False:
+
+            for a in dir_list:
+                self.tree.insert("", "end", values=a)
+            # TODO: Adjust column width to longest filename
+            # TODO: Add size indicator (eg: MB, KB, GB)
+            # self.tree.column("Filename", width=50)
 
 
 class DirFuncs:
 
     def __init__(self):
-        print("NOTHING HERE")
         self.a_list = []
         self.dir_list = []
 
     def directory_list(self, directory):
-        # directory = arg.get()  # get directory from GUI entry box
-        print(directory)
+        del self.a_list[:]
+        del self.dir_list[:]
+
         if True == os.path.isdir(directory):
             dir_path = os.listdir(directory)
             for files in dir_path:
@@ -80,12 +83,12 @@ class DirFuncs:
                 if True == os.path.isfile(fullpath):
                     self.a_list.append([files, int(getFileSize("MB", os.path.getsize(fullpath)))])
             for a in sorted(self.a_list, key=operator.itemgetter(1)):
-                print("{0:>6d}MB  -  {1}".format(a[1], a[0]))
                 self.dir_list.append([a[0], a[1]])
 
             return self.dir_list
         else:
-            print('Not a valid directory.')
+            tk.messagebox.showerror("Error", "Invalid Directory")
+            return False
 
 
 def main():
